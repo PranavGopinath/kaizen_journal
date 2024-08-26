537147eaf1e1fc1f9cfdb3520eb881bc1490784f
PranavGopinath <125942307+PranavGopinath@users.noreply.github.com>
2024-08-26 15:39:42 -0400

    Setup authentication

--- src/app/(auth)/sign-up.jsx
+++ src/app/(auth)/sign-up.jsx
@@ -1,10 +1,23 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Alert} from 'react-native';
 import React, { useState } from 'react';
 import { Link, Stack } from 'expo-router';
+import {supabase} from '../../lib/supabase' 
+import Button from '../../components/Button';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {error} = await supabase.auth.signUp({ email, password })
+    if (error){
+      Alert.alert(error.message)
+    };
+    setLoading(false);
+  }
 
   return (
     <View className = "bg-primary p-20 flex justify-center">
@@ -14,7 +27,6 @@ const SignUp = () => {
       <TextInput
         value={email}
         onChangeText={setEmail}
-        placeholder="johndoe123@gmail.com"
         className = "border-1 border-greyshade p-10 mt-5 mb-20 bg-secondary rounded-sm"
       />
 
@@ -26,7 +38,8 @@ const SignUp = () => {
         className = "border-1 border-greyshade p-10 mt-5 mb-20 bg-secondary rounded-sm"
         secureTextEntry
       />
-      <Link href="/sign-in" className = "self-center font-bold my-10">
+      <Button onPress={signUpWithEmail} disabled={loading} text={loading? 'Creating Account..' : 'Create Account'} />
+      <Link href="/sign-in" className = "self-center font-bold my-12 text-secondary text-lg">
         Sign in
       </Link>
     </View>
