1072bd7b66c4d9e6d5851a83c5757057534f1713
PranavGopinath <125942307+PranavGopinath@users.noreply.github.com>
2024-10-12 16:24:19 -0400

    Conditionally modifying landing page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,10 +8,10 @@ import Auth from '../app/(auth)/Auth'
 import {Session} from '@supabase/supabase-js'
 
 export default function App() {
+  console.log(supabase);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [session, setSession] = useState<Session | null>(null)
-
 
 
   useEffect(() => {
